0852fdbe0bda531be08a3db9b639b0c908b97a9a
HaftamuKebedew <haftamukebedew@gmail.com>
2024-09-20 14:30:22 +0100

    Add files via upload

---
+++ popUp.js
@@ -0,0 +1,32 @@
+'use strict'
+
+
+// function openPopup(){
+//     document.getElementById('popup').classList.add('open-popup');
+// } 
+// function closePopup(){
+//     document.getElementById('popup').classList.remove('open-popup');
+//     // document.getElementById('inputValue').value=""
+// } 
+
+
+
+ function openPopup(){
+   
+    const ppo=document.getElementById('popup');
+    ppo.classList.add('open-popup')
+           } 
+
+      function closePopup(){
+        const ppc=document.getElementById('popup');
+     ppc.classList.remove('open-popup')
+ } 
+
+
+// function openPopup(){
+//     document.getElementById('popup').classList.add('open-popup');
+// } 
+// function closePopup(){
+//     document.getElementById('popup').classList.remove('open-popup');
+//     // document.getElementById('inputValue').value=""
+// } 
